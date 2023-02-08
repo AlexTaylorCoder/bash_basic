cd970d386f604f097eab4630274634ab807380ef
alextaylor <alextaylor515@gmail.com>
2023-02-07 22:39:00 -0500

    Working on more bash basic commands

--- word_counter.sh
+++ word_counter.sh
@@ -1,1 +1,15 @@
-grep user_input in file | wc - w
+
+
+get_input() {
+    echo "Enter files to count"
+    read files 
+    echo "Enter pattern to search"
+    read pattern
+    echo $files
+    for i in $files:
+    do
+        echo $( grep $pattern $i )
+    done
+}
+
+get_input
